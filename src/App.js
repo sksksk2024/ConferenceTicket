8a0eadcc1e6efc2d1dbd0a575775fe60c500f32a
Alex <cota8091@gmail.com>
2025-01-02 17:01:28 +0200

    a bit more more acessible

--- src/App.js
+++ src/App.js
@@ -10,18 +10,21 @@ function App() {
       {/* Backgrounds */}
       <img
         src={BgMobile}
+        aria-label="mobile background"
         className="block sm:hidden -z-30 absolute top-0 left-0 bottom-0 min-h-[2200dvh] w-screen"
-        alt="background"
+        alt="background mobile"
       />
       <img
         src={BgTablet}
+        aria-label="tablet background"
         className="hidden sm:block lg:hidden -z-20 absolute top-0 left-0 bottom-0 min-h-[2200dvh] w-screen"
-        alt="background"
+        alt="background tablet"
       />
       <img
         src={BgDesktop}
+        aria-label="desktop background"
         className="hidden lg:block -z-10 absolute top-0 left-0 w-full min-h-[2200dvh] object-cover"
-        alt="background"
+        alt="background desktop"
       />
       {/* App */}
       <Header />
