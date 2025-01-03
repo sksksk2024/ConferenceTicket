1c3740a606b472b5be7a88f20a610a52860f29a3
Alex <cota8091@gmail.com>
2025-01-03 09:21:01 +0200

    Rethink the background strategy! It's best to put the style in the body directly in Pure CSS(the easiest and best method)

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import Header from './components/Header';
 import Main from './components/Main';
-import BgMobile from './images/background-mobile.png';
-import BgTablet from './images/background-tablet.png';
-import BgDesktop from './images/background-desktop.png';
+// import BgMobile from './images/background-mobile.png';
+// import BgTablet from './images/background-tablet.png';
+// import BgDesktop from './images/background-desktop.png';
 
 function App() {
   return (
     <>
       {/* Backgrounds */}
-      <img
+      {/* <img
         src={BgMobile}
-        className="block sm:hidden -z-30 absolute top-0 left-0 bottom-0 min-h-[2200dvh] w-screen"
+        className="block sm:hidden -z-30 absolute top-0 left-0 bottom-0 min-h-[1000dvh] w-screen object-cover"
         alt=""
         aria-hidden="true"
-      />
-      <img
+      /> */}
+      {/* <img
         src={BgTablet}
-        className="hidden sm:block lg:hidden -z-20 absolute top-0 left-0 bottom-0 min-h-[2200dvh] w-screen"
+        className="hidden sm:block lg:hidden -z-20 absolute top-0 left-0 bottom-0 min-h-[350dvh] w-screen object-cover"
         alt=""
         aria-hidden="true"
-      />
-      <img
+      /> */}
+      {/* <img
         src={BgDesktop}
-        className="hidden lg:block -z-10 absolute top-0 left-0 w-full min-h-[2200dvh] object-cover"
+        className="hidden lg:block -z-10 absolute top-0 left-0 w-screen min-h-[220dvh] object-cover"
         alt=""
         aria-hidden="true"
-      />
+      /> */}
       {/* App */}
       <Header />
       <Main />
